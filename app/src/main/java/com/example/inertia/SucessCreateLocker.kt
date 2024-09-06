c3c6e66534f9a770a8e9d8781ede188e0ef812f8
iagocoelho-deal <iago.coelho@deal.com.br>
2024-09-06 20:53:14 -0300

    Fix sucess page

--- app/src/main/java/com/example/inertia/SucessCreateLocker.kt
+++ app/src/main/java/com/example/inertia/SucessCreateLocker.kt
@@ -1,6 +1,8 @@
 package com.example.inertia
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,10 @@ class SucessCreateLocker : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun handleGoToSearchLocker(view: View) {
+        val intent = Intent(this, SearchLocker::class.java)
+        startActivity(intent)
     }
 }
